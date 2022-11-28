98928a705dd02646eabafeac33819899d333d4e0
R-W-N-Die <110974672+R-W-N-Die@users.noreply.github.com>
2022-11-28 18:43:31 +0300

    Add TailWind

--- src/components/utils/Dialog.vue
+++ src/components/utils/Dialog.vue
@@ -1,6 +1,6 @@
 <template>
   <teleport to="body">
-    <div class="position-absolute npm install vue-tailwind --save w-100 h-100 bg-black d-flex justify-center align-content-center">
+    <div class="absolute bg-black">
       <div class="position-absolute d-flex align-content-lg-space-between border-b-lg">
         <slot name="header"></slot>
         <button class="mdi-cursor-pointer">X</button>
